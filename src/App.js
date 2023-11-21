a2d2a6bec8477fad9424ce2ffab6204bc2198d9d
Anna Maria Lima <87787347+Annmochii@users.noreply.github.com>
2023-11-21 08:27:43 -0300

    new Commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import DashboardPage from './pages/DashboardPage/index.js';
 
 const Stack = createNativeStackNavigator();
 
